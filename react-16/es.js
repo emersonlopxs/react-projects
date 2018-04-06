805510c6389d5bfb381959faff967122a8e9b854
lupuselit <emersonlbr@icloud.com>
2018-04-06 19:58:57 -0300

    add react 16

---
+++ react-16/es.js
@@ -0,0 +1,15 @@
+// let and const 
+
+// const = constant, block scope
+// let = block scope
+
+// Arrow Functions 
+
+function fun(params) {
+  console.log(params);
+}
+
+const func = params => {
+  console.log(params);
+};
+
